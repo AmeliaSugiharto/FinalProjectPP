19c41a8dae08429c764be4033b04f2251f3be18a
Kanaya <kanayasectio@gmail.com>
2024-05-11 23:05:27 +0800

    adding PH in HomePage2

--- src/pages/Homapage2/index.tsx
+++ src/pages/Homapage2/index.tsx
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import React from 'react';
+import {PageHeader} from '../../components';
 
-const index = () => {
+const Homepage = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <PageHeader />
+
+      <View style={styles.contentWrapper} />
+
+      <View style={styles.contentWrapper2} />
+      <View style={styles.contentWrapper3} />
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default Homepage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFD0D0',
+  },
+  contentWrapper: {
+    flex: -1,
+  },
+  contentWrapper2: {
+    flex: 4,
+    backgroundColor: '#FFFFFF',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  },
+  contentWrapper3: {
+    flex: 0.5,
+  },
+});
