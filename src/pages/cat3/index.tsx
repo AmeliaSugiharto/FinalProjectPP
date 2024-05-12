e138962ca43574e665297a9a39bc7c073058f7cb
Avrilkamu2727 <agneskamu31@gmail.com>
2024-05-13 00:08:28 +0800

    adding cat3 25%

--- src/pages/cat3/index.tsx
+++ src/pages/cat3/index.tsx
@@ -1,33 +1,117 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {Gap} from '../../components/atoms';
+import {BackButton} from '../../components/molecules';
 
-const Homepage = () => {
+const Cat3 = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <View style={styles.contentWrapper} />
-      <View style={styles.contentWrapper2} />
-      <View style={styles.contentWrapper3} />
+      <View style={styles.containerImage}>
+        <TouchableOpacity onPress={() => navigation.navigate('Homepage2')}>
+          <BackButton/>
+        </TouchableOpacity>
+        <View style={styles.iconContainer}>
+          <View style={styles.iconContainer1}>
+          </View>
+          <View style={styles.iconContainer2}>
+          </View>
+          <View style={styles.iconContainer3}>
+          </View>
+        </View>
+        <Gap height={32}/>
+        <Gap height={24}/>
+        <View>
+          <TouchableOpacity style={styles.adoptButton} onPress={() => navigation.navigate('DataAdopter')}>
+            <Text style={styles.adoptButtonText}></Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
 
-export default Homepage;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFD0D0',
+    backgroundColor: '#F9DADA', 
   },
-  contentWrapper: {
+  containerImage:{
+    flexDirection: 'column',
     flex: 1,
+    backgroundColor: 'white',
+    height: 830,
+    borderRadius: 50,
+    top: 300,
   },
-  contentWrapper2: {
-    flex: 4,
-    backgroundColor: '#FFFFFF',
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
+  infoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 16,
   },
-  contentWrapper3: {
-    flex: 0.5,
+  iconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginTop: 100,
+  },
+  iconContainer1: {
+    backgroundColor: '#FFD0D0',
+    height: 62,
+    width: 68,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -25,
+    borderBottomWidth: 5,
+    borderColor: 'rgba(0, 0, 0, 0.3)',
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    borderLeftWidth: 0.1,
+    borderRightWidth: 0.1,
+  },
+  iconContainer2: {
+    backgroundColor: '#FFD0D0',
+    height: 62,
+    width: 68,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 5,
+    borderColor: 'rgba(0, 0, 0, 0.3)',
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    borderLeftWidth: 0.1,
+    borderRightWidth: 0.1,
+    marginTop: -25,
+  },
+  iconContainer3: {
+    backgroundColor: '#FFD0D0',
+    height: 62,
+    width: 68,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -25,
+    borderBottomWidth: 5,
+    borderColor: 'rgba(0, 0, 0, 0.3)',
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    borderLeftWidth: 0.1,
+    borderRightWidth: 0.1,
+  },
+  adoptButton: {
+    backgroundColor: '#FFD0D0',
+    borderRadius: 20,
+    paddingVertical: 12,
+    alignItems: 'center',
+    width: 370,
+    marginLeft: 20,
+  },
+  adoptButtonText: {
+    color: 'black',
+    fontWeight: 'bold',
   },
 });
+
+export default Cat3;
