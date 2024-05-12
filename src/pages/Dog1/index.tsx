fc6d2c428121409e9b0196e47b4642ffd7d53dd4
Kanaya <kanayasectio@gmail.com>
2024-05-12 10:45:57 +0800

    adding Gap and Dog1 50%

--- src/pages/Dog1/index.tsx
+++ src/pages/Dog1/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import {View, StyleSheet, TouchableOpacity} from 'react-native';
-import {BackButton} from '../../components/molecules';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { Gap } from '../../components/atoms';
+import { BackButton } from '../../components/molecules';
 
-const Dog1 = ({navigation}) => {
+const Dog1 = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={styles.containerImage}>
         <TouchableOpacity onPress={() => navigation.navigate('Homepage')}>
-          <BackButton/>
+          <BackButton />
         </TouchableOpacity>
+        <View>
+          <Image
+            source={require('../../assets/image/muick.png')}
+            style={styles.dogImage}
+          />
+        </View>
+        <View style={styles.iconContainer}>
+          <View style={styles.iconContainer1}></View>
+          <View style={styles.iconContainer2}></View>
+          <View style={styles.iconContainer3}></View>
+        </View>
+        <Gap height={32} />
+        <Gap height={24} />
+        <View>
+          <TouchableOpacity style={styles.adoptButton} onPress={() => navigation.navigate('DataAdopter')}>
+            <Text style={styles.adoptButtonText}></Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -17,9 +36,9 @@ const Dog1 = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F9DADA', 
+    backgroundColor: '#F9DADA',
   },
-  containerImage:{
+  containerImage: {
     flexDirection: 'column',
     flex: 1,
     backgroundColor: 'white',
@@ -27,7 +46,76 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     top: 300,
   },
-  
+  dogImage: {
+    width: 330,
+    height: 415,
+    bottom: 300,
+    marginLeft: 25,
+    marginRight: 14,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginTop: -250,
+  },
+  iconContainer1: {
+    backgroundColor: '#FFD0D0',
+    height: 62,
+    width: 68,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -25,
+    borderBottomWidth: 5,
+    borderColor: 'rgba(0, 0, 0, 0.3)',
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    borderLeftWidth: 0.1,
+    borderRightWidth: 0.1,
+  },
+  iconContainer2: {
+    backgroundColor: '#FFD0D0',
+    height: 62,
+    width: 68,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 5,
+    borderColor: 'rgba(0, 0, 0, 0.3)',
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    borderLeftWidth: 0.1,
+    borderRightWidth: 0.1,
+    marginTop: -25,
+  },
+  iconContainer3: {
+    backgroundColor: '#FFD0D0',
+    height: 62,
+    width: 68,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -25,
+    borderBottomWidth: 5,
+    borderColor: 'rgba(0, 0, 0, 0.3)',
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    borderLeftWidth: 0.1,
+    borderRightWidth: 0.1,
+  },
+  adoptButton: {
+    backgroundColor: '#FFD0D0',
+    borderRadius: 20,
+    paddingVertical: 12,
+    alignItems: 'center',
+    width: 370,
+    marginLeft: 20,
+  },
+  adoptButtonText: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
 });
 
 export default Dog1;
