fb2debc66e3fd2948563c563217a9c12cae0e4a2
Avrilkamu2727 <agneskamu31@gmail.com>
2024-05-13 00:37:09 +0800

    adding cat3 fixed

--- src/pages/cat3/index.tsx
+++ src/pages/cat3/index.tsx
@@ -135,7 +135,8 @@ const styles = StyleSheet.create({
     fontSize: 15,
     textAlign: 'center',
     color: 'black',
-    
+    textAlign: 'justify', 
+    paddingHorizontal: 10,
   },
   textYears:{
     color: 'black',
