4713d33e658e04939b7c17e0b833eea58e24da49
Kanaya <kanayasectio@gmail.com>
2024-05-12 15:43:06 +0800

    adding Dog3 75%

--- src/pages/Dog3/index.tsx
+++ src/pages/Dog3/index.tsx
@@ -1,22 +1,38 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { Gap } from '../../components/atoms';
-import { BackButton } from '../../components/molecules';
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {Gap} from '../../components/atoms';
+import {BackButton} from '../../components/molecules';
 
-const Dog3 = ({ navigation }) => {
+const Dog3 = ({navigation}) => {
   return (
     <View style={styles.container}>
       <View style={styles.containerImage}>
         <TouchableOpacity onPress={() => navigation.navigate('Homepage')}>
-          <BackButton />
+          <BackButton/>
         </TouchableOpacity>
-        <View style={styles.iconContainer}>
-          <View style={styles.iconContainer1}></View>
-          <View style={styles.iconContainer2}></View>
-          <View style={styles.iconContainer3}></View>
+        <View>
+          <Image
+            source={require('../../assets/image/golden1.png')}
+            style={styles.dogImage}
+          />
         </View>
-        <Gap height={28} />
-        <Gap height={20} />
+        <Text style={styles.dogName}>ARCHI</Text>
+        <View style={styles.iconContainer}>
+          <View style={styles.iconContainer1}>
+            <Image source={require('../../assets/image/male.png')}/>
+            <Text style={styles.iconText}>Sex</Text>
+          </View>
+          <View style={styles.iconContainer2}>
+            <Text style={styles.textYears}>2 years </Text>
+            <Text>Age</Text>
+          </View>
+          <View style={styles.iconContainer3}>
+            <Text style={styles.textKg}>20 Kg </Text>
+            <Text >Weight</Text>
+          </View>
+        </View>
+        <Gap height={28}/>
+        <Gap height={20}/>
         <View>
           <TouchableOpacity style={styles.adoptButton} onPress={() => navigation.navigate('DataAdopter')}>
             <Text style={styles.adoptButtonText}></Text>
@@ -30,9 +46,9 @@ const Dog3 = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F9DADA',
+    backgroundColor: '#F9DADA', 
   },
-  containerImage: {
+  containerImage:{
     flexDirection: 'column',
     flex: 1,
     backgroundColor: 'white',
@@ -40,11 +56,26 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     top: 300,
   },
+  dogImage: {
+    width: 330,
+    height: 415,
+    bottom: 300,
+    marginLeft: 25,
+    marginRight: 14,
+  },
+  dogName: {
+    fontSize: 35,
+    fontFamily: 'Poppins-Bold',
+    color: 'black',
+    textAlign: 'center',
+    bottom: 280,
+    marginTop: -30,
+  },
   iconContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-around',
-    marginTop: 150,
+    marginTop: -250,
   },
   iconContainer1: {
     backgroundColor: '#FFD0D0',
@@ -91,6 +122,12 @@ const styles = StyleSheet.create({
     borderLeftWidth: 0.1,
     borderRightWidth: 0.1,
   },
+  textYears:{
+    color: 'black',
+  },
+  textKg:{
+    color: 'black',
+  },
   adoptButton: {
     backgroundColor: '#FFD0D0',
     borderRadius: 20,
@@ -98,7 +135,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: 370,
     marginLeft: 20,
-    alignSelf: 'center',
+    alignSelf: 'center', 
   },
   adoptButtonText: {
     color: 'black',
