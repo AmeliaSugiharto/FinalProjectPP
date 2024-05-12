58cdfb6c87291313e6c165040ecc4681882fdbf9
Kanaya <kanayasectio@gmail.com>
2024-05-12 13:22:44 +0800

    adding Dog2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import SplashScreen from './SplashScreen';
 import Homepage2 from './Homapage2';
 import Dog1 from './Dog1';
+import Dog2 from './Dog2';
 
-export {SplashScreen, Homepage2, Dog1};
+export {SplashScreen, Homepage2, Dog1, Dog2};
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {SplashScreen, HomePage2, Dog1} from '../pages';
+import {SplashScreen, HomePage2, Dog1, Dog2} from '../pages';
 
 const Stack = createNativeStackNavigator();
 const index = () => {
@@ -19,6 +19,11 @@ const index = () => {
       <Stack.Screen
         name="Dog1"
         component={Dog1}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Dog2"
+        component={Dog2}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
