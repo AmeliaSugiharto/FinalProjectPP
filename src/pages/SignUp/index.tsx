47eac82f67181dce7a27c652d9c6f22bc23612a1
Avrilkamu2727 <agneskamu31@gmail.com>
2024-05-12 20:47:22 +0800

    adding progress 25%

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -18,18 +18,46 @@ const SignUp = ({navigation}) => {
 
   return (
     <View style={styles.container}>
-      <Text> Sign Up</Text>
-      <Text>create your account</Text>
+      <Text style={styles.signupText}>Sign Up</Text>
+      <Text style={styles.instructionsText}>create your account</Text>
 
+      <TextInput
+        placeholder="username"
+        textAlign="center"
+        onChangeText={setUsername}
+        value={username}
+      />
+      <TextInput
+        placeholder="email"
+        textAlign="center"
+        secureTextEntry={true}
+        onChangeText={setPassword}
+        value={password}
+      />
+      <TextInput
+        placeholder="Password"
+        textAlign="center"
+        secureTextEntry={true}
+        onChangeText={setPassword}
+        value={password}
+      />
+      <TextInput
+        placeholder="confirm password"
+        textAlign="center"
+        secureTextEntry={true}
+        onChangeText={setPassword}
+        value={password}
+      />
       <TouchableOpacity
         onPress={() => navigation.navigate('Homepage')}>
-        <Text>CONFIRM</Text>
+        <Text style={styles.buttonText}>CONFIRM</Text>
       </TouchableOpacity>
 
-      <Text>already have any account?</Text>
+      <Text style={styles.noAccountText}>already have any account?</Text>
       <TouchableOpacity
+        style={styles.loginButton2}
         onPress={() => navigation.navigate('SignIn')}>
-        <Text>LOGIN</Text>
+        <Text style={styles.buttonText2}>LOGIN</Text>
       </TouchableOpacity>
     </View>
   );
@@ -42,9 +70,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fff',
   },
+  signupText: {
+    marginTop: 90,
+    marginBottom: -10,
+    fontSize: 35,
+    fontFamily: 'Poppins-SemiBold',
+    color: '#000',
+  },
+  instructionsText: {
+    height: 20,
+    color: '#969292',
+    fontSize: 15,
+    marginBottom: 10,
+  },
+  loginButton2: {
+    backgroundColor: '#704145',
+    borderRadius: 40,
+    fontFamily: 'Poppins-Regular',
+    fontSize: 14,
+    width: 350,
+    height: 48,
+    marginBottom: 86,
+    marginTop: 23,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 20,
+    fontFamily: 'Poppins-Regular',
+  },
   buttonText2: {
     color: '#eee',
     fontSize: 20,
+    fontFamily: 'Poppins-Regular',
+  },
+  noAccountText: {
+    fontSize: 16,
+    marginTop: -20,
+    marginBottom: -20,
     fontFamily: 'Poppins-Regular',
   },
 });
