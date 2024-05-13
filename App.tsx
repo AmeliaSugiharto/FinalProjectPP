6e14e057b7e5b56f099d9d4e1a04a86f97a79021
AmeliaSugiharto <s22210296@student.unklab.ac.id>
2024-05-13 13:05:33 +0800

    adding Firebase SignUp and Improve

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './src/router';
+import './src/config/Firebase';
+import FlashMessage from 'react-native-flash-message';
 
 const App = () => {
   return (
     <NavigationContainer>
       <Router />
+      <FlashMessage position="top" />
     </NavigationContainer>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,35 @@
 import SplashScreen from './SplashScreen';
-import NewPage from './NewPage';
+import GetStarted from './GetStarted';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Homepage from './Homepage';
+import Homepage2 from './Homepage2';
+import Article from './Article';
+import Profile from './Profile';
+import Dog1 from './Dog1';
+import Dog2 from './Dog2';
+import Dog3 from './Dog3';
+import Cat1 from './Cat1';
+import cat2 from './cat2';
+import cat3 from './cat3';
+import DataAdopter from './DataAdopter';
+import Ulasan from './Ulasan';
 
-export {SplashScreen, NewPage};
+export {
+  SplashScreen,
+  GetStarted,
+  SignIn,
+  SignUp,
+  Homepage,
+  Homepage2,
+  Article,
+  Profile,
+  Dog1,
+  Dog2,
+  Dog3,
+  Cat1,
+  cat2,
+  cat3,
+  DataAdopter,
+  Ulasan,
+};
