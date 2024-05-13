f701ab247516822ab7a857c22dcb10ab22920efd
AmeliaSugiharto <s22210296@student.unklab.ac.id>
2024-05-13 09:07:43 +0800

    adding Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
 import SplashScreen from './SplashScreen';
+import GetStarted from './GetStarted';
+import SignIn from './SignIn';
 import Homepage from './Homepage';
+import Homepage2 from './Homepage2';
+import Article from './Article';
 import Profile from './Profile';
+import Dog1 from './Dog1';
+import Dog2 from './Dog2';
+import Dog3 from './Dog3';
+import Cat1 from './Cat1';
+import DataAdopter from './DataAdopter';
+import Ulasan from './Ulasan';
 
-export {SplashScreen, Homepage, Profile};
+export {
+  SplashScreen,
+  GetStarted,
+  SignIn,
+  Homepage,
+  Homepage2,
+  Article,
+  Profile,
+  Dog1,
+  Dog2,
+  Dog3,
+  Cat1,
+  DataAdopter,
+  Ulasan,
+};
