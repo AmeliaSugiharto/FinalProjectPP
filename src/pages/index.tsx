084c89e79ac3d2069f8d3081f187b450a73661c3
Avrilkamu2727 <agneskamu31@gmail.com>
2024-05-13 09:56:40 +0800

    SplashScreen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import SplashScreen from './SplashScreen';
 import SignUp from './SignUp';
 import Cat2  from './Cat2';
 import Cat3  from './Cat3';
 
 export {
+    SplashScreen,
     SignUp,
     Cat2,
     Cat3,
